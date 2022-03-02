d6e244f6bf5f8db977ffdbcf384c27e4d0c26f77
Jorge Falcón <jorgef_campos@outlook.com>
2022-03-01 21:00:23 -0600

    added validation for unique category

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,3 +1,4 @@
+from asyncio import constants
 from tabnanny import verbose
 from tokenize import Triple
 from users.models import User
@@ -10,7 +11,7 @@ class Category(m.Model):
     name = m.CharField(max_length=30, null=False, blank=False)
     owner = m.ForeignKey(User, on_delete=m.CASCADE, related_name="owner", null=False, blank=False)
     description = m.CharField(max_length=150, null=True, blank=True)
-    slug = m.SlugField(unique=True, null=False, blank=True)
+    slug = m.SlugField(unique=False, null=False, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -24,5 +25,12 @@ def set_slug(sender, instance, *args, **kwargs):
     # if not instance.slug:
     instance.slug = slugify(instance.name)
 
+# def verify_name(sender, instance, *args, **kwargs):
+#     print(.owner)
+#     # lol = Category.objects.filter(owner=self.user)
+#     # if instance.name:
+#         # print("abr")
+
 
 pre_save.connect(set_slug, sender=Category)
+# pre_save.connect(verify_name, sender=Category)
