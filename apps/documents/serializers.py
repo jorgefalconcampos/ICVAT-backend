67cd7e598ab01a3ad136c0f00c5612fe5a629ed5
Jorge Falcón <jorgef_campos@outlook.com>
2022-03-05 23:15:33 -0600

    changes

--- apps/documents/serializers.py
+++ apps/documents/serializers.py
@@ -1,9 +1,11 @@
+from unicodedata import category
 from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 from rest_framework import serializers
 from . models import Document
 
 class DocumentSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
+    category = serializers.CharField(source="category.name")
     class Meta:
         model = Document
         fields = ['document_id', 'author', 'title', 'category', 'body', 'tags', 'created_date']
